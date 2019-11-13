070c187ee5618b5314313ea8c83a91346599ad67
Prüter <fprueter@isv.local>
2019-11-13 21:15:01 +0100

    disable unauthenticated access to the dav server

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+# do not allow unauthenticated access to prevent errors in relation with the carddac roundcube plugin that uses cURL internally
+# see this issue: https://github.com/blind-coder/rcmcarddav/issues/188
+sed -i 's/public \$allowUnauthenticatedAccess = true;/public \$allowUnauthenticatedAccess = false;/' /var/www/baikal/vendor/sabre/dav/lib/DAVACL/Plugin.php
+
 php-fpm7
 exec nginx
