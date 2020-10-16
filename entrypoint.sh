b02bc65f9c7e720e77e00c0bc2276b438062fcf2
felix <felix@grupr.de>
2020-10-16 20:20:10 +0000

    correct typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# do not allow unauthenticated access to prevent errors in relation with the carddac roundcube plugin that uses cURL internally
+# do not allow unauthenticated access to prevent errors in relation with the carddav roundcube plugin that uses cURL internally
 # see this issue: https://github.com/blind-coder/rcmcarddav/issues/188
 sed -i 's/public \$allowUnauthenticatedAccess = true;/public \$allowUnauthenticatedAccess = false;/' /var/www/baikal/vendor/sabre/dav/lib/DAVACL/Plugin.php
 
