112b6f477b2f12955f144094da630f47eb063e11
Felix <felixfkrull@users.noreply.github.com>
2022-10-18 23:00:07 +0200

    enlarge PHP memory limit

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 # do not allow unauthenticated access to prevent errors in relation with the carddav roundcube plugin that uses cURL internally
 # see this issue: https://github.com/blind-coder/rcmcarddav/issues/188
 sed -i 's/public \$allowUnauthenticatedAccess = true;/public \$allowUnauthenticatedAccess = false;/' /var/www/baikal/vendor/sabre/dav/lib/DAVACL/Plugin.php
+sed -i 's/memory_limit = 128M/memory_limit = 256M/' /etc/php7/php.ini
 
 php-fpm7
 exec nginx
